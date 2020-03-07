d26d3f6445daff818a5ab639aafeda4b2cbbc0e6
iskonplus <58971909+iskonplus@users.noreply.github.com>
2020-03-08 00:47:08 +0300

    init: start brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let a = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    (str[i]) === ")" && (a-=1);
+    (str[i]) === "]" && (a-=1);
+    (str[i]) === "}" && (a-=1);
+    (str[i]) === "(" && (a += 1);
+    (str[i]) === "[" && (a += 1);
+    (str[i]) === "{" && (a += 1);
+    if (a < 0) { return !a } 
+      if (a < 0) { return !a } 
+  }
+  return !a    
 }
